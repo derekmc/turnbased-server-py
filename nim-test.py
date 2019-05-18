2afccf129fa969b70a1888fa1351a493040ca490
Derek McDaniel <derek7mc@gmail.com>
2019-05-18 06:46:53 -0600

    added assert

--- nim-test.py
+++ nim-test.py
@@ -7,15 +7,9 @@ GameScoreSchema = {
     "type": "object"
 }
 
-
-
-
 class NimTest(unittest.TestCase):
-
     def test(self):
-
         # print("Testing Game:", handler.GameParadigm, "version:", handler.Version)
-
         handler.verify(
             loads('{"stones": 10, "turn":1}'),
             loads('{"take":2}'),
@@ -25,13 +19,14 @@ class NimTest(unittest.TestCase):
             loads('{"stones": 10, "turn":1}'),
             loads('{"take":2}'),
             seat=2)
-        
-        
 
+        self.assertRaises(AssertionError, handler.verify,
+            loads('{"stones": 10, "turn":2}'),
+            loads('{"take":3}'),
+            seat=2)
+         
 def test():
     unittest.main(exit=False)
 
 test()
-
-
 
