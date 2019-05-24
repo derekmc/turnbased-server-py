f316ffb76aefb6e66fd0dc81422266f535c5dbc8
Derek McDaniel <derek7mc@gmail.com>
2019-05-24 12:06:07 -0600

    updated documentation page

--- pages.py
+++ pages.py
@@ -26,20 +26,46 @@ doc_src = """
   <head>
   </head>
   <body>
-    <h1> Documentation Page </h1>
-    What the workflow will look like:
-    <ol>
-     <li> Automatic Session (with cookies)</li>
-     <li> Make a game or Join a Game
-       <ol>
-         <li> Make a game
-           <ul><li>item</li></ul></li>
-         <li> Join a game
-           <ul><li>item</li></ul></li>
-       </ol>
-     </li>
-     <li> Join as Developer </li>
-    </ol>
+<pre>
+Documentation Page
+=========================
+
+What the workflow will look like:
+
+ - Automatic Session (with cookies)
+
+ - Make a game or Join a Game
+    Make a game
+    Join a game
+
+ - Developer Info
+
+ - Paradigm Info:
+    min_players
+    max_players
+    sequenced_turns --
+      Enforce turn sequence.
+      Turns must be sequenced to impose time limits.
+      Once time runs out, a "null" move is sent.
+    elo: boolean
+
+ - Game Info:
+    min_players
+    max_players
+    turn_time --
+      time allocated for each move, including first.
+    total_time --
+      time allocated per player for entire game.
+      once the turn time has been used, the total time starts draining.
+    public_list -- list the game so anyone can view or join.
+    public_view -- spectators may join, and game is listed after it is started.
+
+    TODO: update elo based on score.
+    Elo Stuff
+      unranked -- dont compute elo
+      min_elo
+      max_elo
+</pre>
   </body>
 </html>
 """
