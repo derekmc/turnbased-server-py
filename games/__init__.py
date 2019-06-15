35f26124414ca418866583d8e8e4303ae26762f4
Derek McDaniel <derek7mc@gmail.com>
2019-06-15 09:14:07 -0600

    fixed importing from games folder

--- games/__init__.py
+++ games/__init__.py
@@ -1,4 +1,5 @@
 
+"""
 import os
 
 for module in os.listdir(os.path.dirname(__file__)):
@@ -6,3 +7,16 @@ for module in os.listdir(os.path.dirname(__file__)):
         continue
     __import__(module[:-3], locals(), globals())
 del module
+"""
+
+import os
+import glob
+import importlib
+try:
+    __games_dir = os.path.basename(os.path.normpath(os.path.dirname(os.path.realpath(__file__))))
+except:
+    __games_dir = "games"
+modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
+games_list = [ importlib.import_module(__games_dir + "." + os.path.basename(f)[:-3]) for f in modules if os.path.isfile(f) and not f.endswith('__init__.py')]
+
+
