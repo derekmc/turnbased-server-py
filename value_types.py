dfc1cd4cf6e77110ffc99f9d53740d1f77730841
- <derek7mc@gmail.com>
2019-07-02 11:57:08 -0600

    working on value types.

--- value_types.py
+++ value_types.py
@@ -5,43 +5,52 @@ definitions = enum.Enum("definitions", "ANY")
 ANY = definitions.ANY
 
 def typecheck(*arg):
-    if len(arg) == 2:
-        return typecheck_single(arg[0], arg[1])
+    if len(arg) < 2:
+        raise ArgumentError("value types typecheck requires 2 arguments or more.")
     types = arg[:-1]
     value = arg[-1]
     for t in types:
-        t = arg[i]
-        try:
-            return typecheck_single(example, value)
-        except:
-            pass
-    raise TypeError("value_types value:" + str(value) + " types: " + types)
+        if typecheck_single(t, value):
+            return value
+    raise TypeError("--Value Types-- value " + str(value) + ", did not match any of types " + str(types))
 
 def typecheck_single(example, value):
     if example == None: # match anything
         return value
-    example_type = type(example)
-    t = type(value)
     
-    if example_type is str:
-        return t is str
-    if example_type is int:
-        return isinstance(t, int)
-    if example_type is long:
-        return isinstance(t, long)
-    if example_type is float:
-        return isinstance(t, float)
-    if example_type is complex:
-        return is_instance(t, complex)
+    if isinstance(example, str):
+        return isinstance(value, str)
+    if isinstance(example, int):
+        return isinstance(value, int)
+    if isinstance(example, float):
+        return isinstance(value, float)
+    if isinstance(example, complex):
+        return isinstance(value, complex)
     
-    if example_type is list:
-        if not is_instance(t, list):
+    if isinstance(example, tuple):
+        if not isinstance(value, tuple):
             return False
+        l = len(example)
+        return l == len(value)
+    if isinstance(example, list):
+        if not isinstance(value, list):
+            return False
+        l = len(example)
+        return l ==1 or l == len(value)
+    if isinstance(example, dict):
+        if not isinstance(value, dict):
+            return False
+        return True
+
         
-     
-    
 def test():
-    pass
+    T = typecheck
+    T(0, 0)
+    T(0, 5)
+    T(0, -5)
+    T(0.0, 3.14159263)
+    T('', 'test')
+    print("Value Types tests finished.")
     
     
 if __name__ == "__main__":
